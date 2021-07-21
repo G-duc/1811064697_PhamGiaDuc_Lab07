5f3357a8ae9b0273b04dbd8c3aa866fee0129a23
G-duc <gduc1511@gmail.com>
2021-07-21 12:55:28 +0700

    Add project files.

---
+++ WEB_API_LAB07/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WEB_API_LAB07.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WEB_API_LAB07/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WEB_API_LAB07.WebApiApplication" Language="C#" %>
